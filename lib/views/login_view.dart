e37eb961989055b954b973dfbfaa90eae9c9f284
bidingurusmk <bidingurusmk@gmail.com>
2024-09-23 11:57:44 +0700

    master

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -13,6 +13,18 @@ class LoginView extends StatelessWidget {
         ElevatedButton(onPressed: (){
           Navigator.pushReplacementNamed(context, '/profile');
         }, child: Text("Profile"),),
+        ElevatedButton(onPressed: (){
+          Navigator.pushNamed(context, '/transksi',arguments: [{
+            "id":1,
+            "nama_produk":"meja",
+            "harga":5000,
+          },
+          {
+            "id":2,
+            "nama_produk":"kursi",
+            "harga":10000,
+          }]);
+        }, child: Text("Transaksi"),),
       ],
     );
   }
